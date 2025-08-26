e7f4b47fc0284127e4c5835aca3973622628a794
jordanchicksen1 <2333640@students.wits.ac.za>
2025-08-26 19:00:35 +0200

    doing game selection

---
+++ COLOUR_CHASER/Assets/scripts/gameSelection/gameSelector.cs
@@ -0,0 +1,164 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gameSelector : MonoBehaviour
+{
+    public GameObject gamesGroup;
+    public GameObject game1Page;
+    public GameObject game2Page;
+    public GameObject game3Page;
+    public GameObject game4Page;
+    public GameObject game5Page;
+
+    public void Game1()
+    {
+        gamesGroup.SetActive(false);
+        game1Page.SetActive(true);
+        game2Page.SetActive(false);
+        game3Page.SetActive(false);
+        game4Page.SetActive(false);
+        game5Page.SetActive(false);
+    }
+    public void Game2() 
+    {
+        gamesGroup.SetActive(false);
+        game1Page.SetActive(false);
+        game2Page.SetActive(true);
+        game3Page.SetActive(false);
+        game4Page.SetActive(false);
+        game5Page.SetActive(false);
+    }
+    public void Game3() 
+    {
+        gamesGroup.SetActive(false);
+        game1Page.SetActive(false);
+        game2Page.SetActive(false);
+        game3Page.SetActive(true);
+        game4Page.SetActive(false);
+        game5Page.SetActive(false);
+    }
+    public void Game4() 
+    {
+        gamesGroup.SetActive(false);
+        game1Page.SetActive(false);
+        game2Page.SetActive(false);
+        game3Page.SetActive(false);
+        game4Page.SetActive(true);
+        game5Page.SetActive(false);
+    }
+    public void Game5() 
+    {
+        gamesGroup.SetActive(false);
+        game1Page.SetActive(false);
+        game2Page.SetActive(false);
+        game3Page.SetActive(false);
+        game4Page.SetActive(false);
+        game5Page.SetActive(true);
+    }
+
+    public void ExitPage()
+    {
+        gamesGroup.SetActive(true);
+        game1Page.SetActive(false);
+        game2Page.SetActive(false);
+        game3Page.SetActive(false);
+        game4Page.SetActive(false);
+        game5Page.SetActive(false);
+    }
+
+    public void nextGame1()
+    {
+        gamesGroup.SetActive(false);
+        game1Page.SetActive(false);
+        game2Page.SetActive(true);
+        game3Page.SetActive(false);
+        game4Page.SetActive(false);
+        game5Page.SetActive(false);
+    }
+    public void nextGame2() 
+    {
+        gamesGroup.SetActive(false);
+        game1Page.SetActive(false);
+        game2Page.SetActive(false);
+        game3Page.SetActive(true);
+        game4Page.SetActive(false);
+        game5Page.SetActive(false);
+    }
+    public void nextGame3() 
+    {
+        gamesGroup.SetActive(false);
+        game1Page.SetActive(false);
+        game2Page.SetActive(false);
+        game3Page.SetActive(false);
+        game4Page.SetActive(true);
+        game5Page.SetActive(false);
+    }
+    public void nextGame4() 
+    {
+        gamesGroup.SetActive(false);
+        game1Page.SetActive(false);
+        game2Page.SetActive(false);
+        game3Page.SetActive(false);
+        game4Page.SetActive(false);
+        game5Page.SetActive(true);
+    }
+    public void nextGame5() 
+    {
+        gamesGroup.SetActive(false);
+        game1Page.SetActive(true);
+        game2Page.SetActive(false);
+        game3Page.SetActive(false);
+        game4Page.SetActive(false);
+        game5Page.SetActive(false);
+    }
+
+    public void prevGame1()
+    {
+        gamesGroup.SetActive(false);
+        game1Page.SetActive(false);
+        game2Page.SetActive(false);
+        game3Page.SetActive(false);
+        game4Page.SetActive(false);
+        game5Page.SetActive(true);
+    }
+    public void prevGame2() 
+    {
+        gamesGroup.SetActive(false);
+        game1Page.SetActive(true);
+        game2Page.SetActive(false);
+        game3Page.SetActive(false);
+        game4Page.SetActive(false);
+        game5Page.SetActive(false);
+    }
+    public void prevGame3() 
+    {
+        gamesGroup.SetActive(false);
+        game1Page.SetActive(false);
+        game2Page.SetActive(true);
+        game3Page.SetActive(false);
+        game4Page.SetActive(false);
+        game5Page.SetActive(false);
+    }
+    public void prevGame4() 
+    {
+        gamesGroup.SetActive(false);
+        game1Page.SetActive(false);
+        game2Page.SetActive(false);
+        game3Page.SetActive(true);
+        game4Page.SetActive(false);
+        game5Page.SetActive(false);
+    }
+    public void prevGame5() 
+    {
+        gamesGroup.SetActive(false);
+        game1Page.SetActive(false);
+        game2Page.SetActive(false);
+        game3Page.SetActive(false);
+        game4Page.SetActive(true);
+        game5Page.SetActive(false);
+    }
+
+
+
+}
