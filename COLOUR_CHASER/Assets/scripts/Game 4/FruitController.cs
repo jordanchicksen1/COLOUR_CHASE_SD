db77db29b1e19c16afaa75f88a88cad312cb42f4
lxrdflame <tyrique.s.r@gmail.com>
2025-09-11 12:03:59 +0200

    Try this

--- COLOUR_CHASER/Assets/scripts/Game 4/FruitController.cs
+++ COLOUR_CHASER/Assets/scripts/Game 4/FruitController.cs
@@ -129,8 +129,6 @@ public class FruitManager : MonoBehaviour
             //transform.position = OGPosotion;
             if (!isOnConveyor)
             {
-                rb.velocity = Vector2.zero;
-                rb.gravityScale = 1;
             }
         }
         else
