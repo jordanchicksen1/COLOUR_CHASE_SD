70c56a75fffb4d44e6d6f44d7fadceeaa8b4d2a7
jordanchicksen1 <2333640@students.wits.ac.za>
2025-10-03 15:46:41 +0200

    ui is mostly done

--- COLOUR_CHASER/Assets/scripts/gameSelection/gameSelector.cs
+++ COLOUR_CHASER/Assets/scripts/gameSelection/gameSelector.cs
@@ -7,19 +7,25 @@ using UnityEngine.EventSystems;
 public class gameSelector : MonoBehaviour
 {
     public GameObject gamesGroup;
+    public GameObject gamesGroup2;
     public GameObject game1Page;
     public GameObject game2Page;
     public GameObject game3Page;
     public GameObject game4Page;
     public GameObject game5Page;
+    public GameObject game6Page;
+    public GameObject game7Page;
 
     //button selection stuff
     public GameObject defaultGamesGroupButton;
+    public GameObject defaultGamesGroup2Button;
     public GameObject defaultGame1Button;
     public GameObject defaultGame2Button;
     public GameObject defaultGame3Button;
     public GameObject defaultGame4Button;
     public GameObject defaultGame5Button;
+    public GameObject defaultGame6Button;
+    public GameObject defaultGame7Button;
 
     public AudioSource clickSFX;
 
@@ -36,11 +42,14 @@ public class gameSelector : MonoBehaviour
     public void Game1()
     {
         gamesGroup.SetActive(false);
+        gamesGroup2.SetActive(false);
         game1Page.SetActive(true);
         game2Page.SetActive(false);
         game3Page.SetActive(false);
         game4Page.SetActive(false);
         game5Page.SetActive(false);
+        game6Page.SetActive(false);
+        game7Page.SetActive(false);
         clickSFX.Play();
 
         SetSelected(defaultGame1Button);
@@ -48,11 +57,14 @@ public class gameSelector : MonoBehaviour
     public void Game2() 
     {
         gamesGroup.SetActive(false);
+        gamesGroup2.SetActive(false);
         game1Page.SetActive(false);
         game2Page.SetActive(true);
         game3Page.SetActive(false);
         game4Page.SetActive(false);
         game5Page.SetActive(false);
+        game6Page.SetActive(false);
+        game7Page.SetActive(false);
         clickSFX.Play();
 
         SetSelected(defaultGame2Button);
@@ -60,11 +72,14 @@ public class gameSelector : MonoBehaviour
     public void Game3() 
     {
         gamesGroup.SetActive(false);
+        gamesGroup2.SetActive(false);
         game1Page.SetActive(false);
         game2Page.SetActive(false);
         game3Page.SetActive(true);
         game4Page.SetActive(false);
         game5Page.SetActive(false);
+        game6Page.SetActive(false);
+        game7Page.SetActive(false);
         clickSFX.Play();
 
         SetSelected(defaultGame3Button);
@@ -72,11 +87,14 @@ public class gameSelector : MonoBehaviour
     public void Game4() 
     {
         gamesGroup.SetActive(false);
+        gamesGroup2.SetActive(false);
         game1Page.SetActive(false);
         game2Page.SetActive(false);
         game3Page.SetActive(false);
         game4Page.SetActive(true);
         game5Page.SetActive(false);
+        game6Page.SetActive(false);
+        game7Page.SetActive(false);
         clickSFX.Play();
 
         SetSelected(defaultGame4Button);
@@ -84,27 +102,81 @@ public class gameSelector : MonoBehaviour
     public void Game5() 
     {
         gamesGroup.SetActive(false);
+        gamesGroup2.SetActive(false);
         game1Page.SetActive(false);
         game2Page.SetActive(false);
         game3Page.SetActive(false);
         game4Page.SetActive(false);
         game5Page.SetActive(true);
+        game6Page.SetActive(false);
+        game7Page.SetActive(false);
         clickSFX.Play();
 
         SetSelected(defaultGame5Button);
     }
 
-    public void ExitPage()
+    public void Game6()
     {
-        gamesGroup.SetActive(true);
+        gamesGroup2.SetActive(false);
+        gamesGroup2.SetActive(false);
         game1Page.SetActive(false);
         game2Page.SetActive(false);
         game3Page.SetActive(false);
         game4Page.SetActive(false);
         game5Page.SetActive(false);
+        game6Page.SetActive(true);
+        game7Page.SetActive(false);
+        clickSFX.Play();
+
+        SetSelected(defaultGame6Button);
+    }
+
+    public void Game7()
+    {
+        gamesGroup2.SetActive(false);
+        gamesGroup.SetActive(false);
+        game1Page.SetActive(false);
+        game2Page.SetActive(false);
+        game3Page.SetActive(false);
+        game4Page.SetActive(false);
+        game5Page.SetActive(false);
+        game6Page.SetActive(false);
+        game7Page.SetActive(true);
+        clickSFX.Play();
+
+        SetSelected(defaultGame7Button);
+    }
+
+    public void ExitPage()
+    {
+        gamesGroup.SetActive(true);
+        gamesGroup2.SetActive(false);
+        game1Page.SetActive(false);
+        game2Page.SetActive(false);
+        game3Page.SetActive(false);
+        game4Page.SetActive(false);
+        game5Page.SetActive(false);
+        game6Page.SetActive(false);
+        game7Page.SetActive(false);
         clickSFX.Play();
         
         SetSelected(defaultGamesGroupButton);
+    }
+
+    public void ExitPage2()
+    {
+        gamesGroup.SetActive(false);
+        gamesGroup2.SetActive(true);
+        game1Page.SetActive(false);
+        game2Page.SetActive(false);
+        game3Page.SetActive(false);
+        game4Page.SetActive(false);
+        game5Page.SetActive(false);
+        game6Page.SetActive(false);
+        game7Page.SetActive(false);
+        clickSFX.Play();
+
+        SetSelected(defaultGamesGroup2Button);
     }
 
     public void nextGame1()
@@ -115,6 +187,8 @@ public class gameSelector : MonoBehaviour
         game3Page.SetActive(false);
         game4Page.SetActive(false);
         game5Page.SetActive(false);
+        game6Page.SetActive(false);
+        game7Page.SetActive(false);
         clickSFX.Play();
         SetSelected(defaultGame2Button);
     }
@@ -126,6 +200,8 @@ public class gameSelector : MonoBehaviour
         game3Page.SetActive(true);
         game4Page.SetActive(false);
         game5Page.SetActive(false);
+        game6Page.SetActive(false);
+        game7Page.SetActive(false);
         clickSFX.Play();
         SetSelected(defaultGame3Button);
     }
@@ -137,6 +213,8 @@ public class gameSelector : MonoBehaviour
         game3Page.SetActive(false);
         game4Page.SetActive(true);
         game5Page.SetActive(false);
+        game6Page.SetActive(false);
+        game7Page.SetActive(false);
         clickSFX.Play();
         SetSelected(defaultGame4Button);
     }
@@ -148,17 +226,49 @@ public class gameSelector : MonoBehaviour
         game3Page.SetActive(false);
         game4Page.SetActive(false);
         game5Page.SetActive(true);
+        game6Page.SetActive(false);
+        game7Page.SetActive(false);
         clickSFX.Play();   
         SetSelected(defaultGame5Button);
     }
     public void nextGame5() 
     {
         gamesGroup.SetActive(false);
-        game1Page.SetActive(true);
+        game1Page.SetActive(false);
         game2Page.SetActive(false);
         game3Page.SetActive(false);
         game4Page.SetActive(false);
         game5Page.SetActive(false);
+        game6Page.SetActive(true);
+        game7Page.SetActive(false);
+        clickSFX.Play();
+        SetSelected(defaultGame6Button);
+    }
+
+    public void nextGame6()
+    {
+        gamesGroup.SetActive(false);
+        game1Page.SetActive(false);
+        game2Page.SetActive(false);
+        game3Page.SetActive(false);
+        game4Page.SetActive(false);
+        game5Page.SetActive(false);
+        game6Page.SetActive(false);
+        game7Page.SetActive(true);
+        clickSFX.Play();
+        SetSelected(defaultGame7Button);
+    }
+
+    public void nextGame7()
+    {
+        gamesGroup.SetActive(true);
+        game1Page.SetActive(false);
+        game2Page.SetActive(false);
+        game3Page.SetActive(false);
+        game4Page.SetActive(false);
+        game5Page.SetActive(false);
+        game6Page.SetActive(false);
+        game7Page.SetActive(false);
         clickSFX.Play();
         SetSelected(defaultGame1Button);
     }
@@ -170,9 +280,11 @@ public class gameSelector : MonoBehaviour
         game2Page.SetActive(false);
         game3Page.SetActive(false);
         game4Page.SetActive(false);
-        game5Page.SetActive(true);
+        game5Page.SetActive(false);
+        game6Page.SetActive(false);
+        game7Page.SetActive(true);
         clickSFX.Play();
-        SetSelected(defaultGame5Button);
+        SetSelected(defaultGame7Button);
     }
     public void prevGame2() 
     {
@@ -182,6 +294,8 @@ public class gameSelector : MonoBehaviour
         game3Page.SetActive(false);
         game4Page.SetActive(false);
         game5Page.SetActive(false);
+        game6Page.SetActive(false);
+        game7Page.SetActive(false);
         clickSFX.Play();
         SetSelected(defaultGame1Button);
     }
@@ -193,6 +307,8 @@ public class gameSelector : MonoBehaviour
         game3Page.SetActive(false);
         game4Page.SetActive(false);
         game5Page.SetActive(false);
+        game6Page.SetActive(false);
+        game7Page.SetActive(false);
         clickSFX.Play();
         SetSelected(defaultGame2Button);
     }
@@ -204,6 +320,8 @@ public class gameSelector : MonoBehaviour
         game3Page.SetActive(true);
         game4Page.SetActive(false);
         game5Page.SetActive(false);
+        game6Page.SetActive(false);
+        game7Page.SetActive(false);
         clickSFX.Play();
         SetSelected(defaultGame3Button);
     }
@@ -215,9 +333,73 @@ public class gameSelector : MonoBehaviour
         game3Page.SetActive(false);
         game4Page.SetActive(true);
         game5Page.SetActive(false);
+        game6Page.SetActive(false);
+        game7Page.SetActive(false);
         clickSFX.Play();
         SetSelected(defaultGame4Button);
     }
+
+    public void prevGame6()
+    {
+        gamesGroup.SetActive(false);
+        game1Page.SetActive(false);
+        game2Page.SetActive(false);
+        game3Page.SetActive(false);
+        game4Page.SetActive(false);
+        game5Page.SetActive(true);
+        game6Page.SetActive(false);
+        game7Page.SetActive(false);
+        clickSFX.Play();
+        SetSelected(defaultGame5Button);
+    }
+
+    public void prevGame7()
+    {
+        gamesGroup.SetActive(false);
+        game1Page.SetActive(false);
+        game2Page.SetActive(false);
+        game3Page.SetActive(false);
+        game4Page.SetActive(false);
+        game5Page.SetActive(false);
+        game6Page.SetActive(true);
+        game7Page.SetActive(false);
+        clickSFX.Play();
+        SetSelected(defaultGame6Button);
+    }
+
+    public void nextGameGroup1()
+    {
+        gamesGroup.SetActive(false);
+        gamesGroup2.SetActive(true);
+
+        SetSelected(defaultGamesGroup2Button);
+    }
+
+    public void previousGameGroup1()
+    {
+        gamesGroup.SetActive(false);
+        gamesGroup2.SetActive(true);
+
+        SetSelected(defaultGamesGroup2Button);
+    }
+
+    public void nextGameGroup2()
+    {
+        gamesGroup.SetActive(true);
+        gamesGroup2.SetActive(false);
+
+        SetSelected(defaultGamesGroupButton);
+    }
+
+    public void previousGameGroup2()
+    {
+        gamesGroup.SetActive(true);
+        gamesGroup2.SetActive(false);
+
+        SetSelected(defaultGamesGroupButton);
+    }
+
+
 
     public void QuitGame()
     {
@@ -246,5 +428,15 @@ public class gameSelector : MonoBehaviour
     public void PlayGame5()
     {
         SceneManager.LoadScene("Game3");
+    }
+
+    public void PlayGame6()
+    {
+        SceneManager.LoadScene("Game 6");
+    }
+
+    public void PlayGame7()
+    {
+        SceneManager.LoadScene("Game7");
     }
 }
