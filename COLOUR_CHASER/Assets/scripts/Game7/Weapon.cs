50c76b959ba313052e176453be74c428b6999f3b
profoundBoi <connor170103@gmail.com>
2025-10-03 16:30:51 +0200

    started weapon system

---
+++ COLOUR_CHASER/Assets/scripts/Game7/Weapon.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    [SerializeField]
+    public GameObject bubbleBlaster;
+    public GameObject lazerGun;
+    public GameObject pumpshotGun;
+    public int fireRate;
+    public int recoil;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    void LazerGun()
+    {
+
+    }
+
+    void BubbleBlaster()
+    {
+
+    }
+
+    void PumpShotGun()
+    {
+
+    }
+}
