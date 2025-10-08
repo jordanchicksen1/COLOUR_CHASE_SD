a219269e40382c2add6b588678acafe22759c756
profoundBoi <connor170103@gmail.com>
2025-10-08 21:04:12 +0200

    Score System

--- COLOUR_CHASER/Assets/scripts/Game7/KillBox.cs
+++ COLOUR_CHASER/Assets/scripts/Game7/KillBox.cs
@@ -1,18 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class KillBox : MonoBehaviour
 {
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.CompareTag("Player1") || collision.CompareTag("Player2"))
+        if (collision.CompareTag("Player1"))
         {
-            BrawlerController player = collision.GetComponent<BrawlerController>();
-            if (player != null)
-            {
-                player.RespawnToSpawn();
-            }
+            ScoreManager.Instance.AddScoreToPlayer2();
+        }
+        else if (collision.CompareTag("Player2"))
+        {
+            ScoreManager.Instance.AddScoreToPlayer1();
+        }
+
+        BrawlerController player = collision.GetComponent<BrawlerController>();
+        if (player != null)
+        {
+            player.RespawnToSpawn();
         }
     }
 }
---
+++ COLOUR_CHASER/Assets/scripts/Game7/ScoreManager.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class ScoreManager : MonoBehaviour
+{
+    public static ScoreManager Instance;
+
+    public int player1Score = 0;
+    public int player2Score = 0;
+
+    [Header("UI References")]
+    public TMP_Text player1ScoreText;
+    public TMP_Text player2ScoreText;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void Start()
+    {
+        UpdateUI();
+    }
+
+    public void AddScoreToPlayer1()
+    {
+        player1Score++;
+        UpdateUI();
+    }
+
+    public void AddScoreToPlayer2()
+    {
+        player2Score++;
+        UpdateUI();
+    }
+
+    private void UpdateUI()
+    {
+        if (player1ScoreText != null)
+            player1ScoreText.text = "" + player1Score;
+        if (player2ScoreText != null)
+            player2ScoreText.text = "" + player2Score;
+    }
+}
