f63fa6769909c65e863ae042c6eab2de4f4b0957
jordanchicksen1 <2333640@students.wits.ac.za>
2025-10-09 13:46:04 +0200

    connor game finished

--- COLOUR_CHASER/Assets/scripts/Game 4/GameData.cs
+++ COLOUR_CHASER/Assets/scripts/Game 4/GameData.cs
@@ -4,7 +4,14 @@ using UnityEngine;
 
 public static class GameData 
 {
+   //fruit game
    public static int playerScore = 0;
+
+    //platformer game
    public static int coinScore = 0;
+
+    //smash bros game
+    public static int playerOneScore = 0;
+    public static int playerTwoScore = 0;
 
 }
---
+++ COLOUR_CHASER/Assets/scripts/Game7/resultsScreenP2Wins.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class resultsScreenP2Wins : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI resultsScoreText;
+    [SerializeField] private float countSpeed = 1f; // smaller = faster
+    [SerializeField] private AudioSource countSound;  // optional: little ticking sound
+
+    private void Start()
+    {
+        StartCoroutine(AnimateFinalScore());
+    }
+
+    private IEnumerator AnimateFinalScore()
+    {
+        int finalScore = GameData.playerTwoScore;
+        int displayedScore = 0;
+
+        while (displayedScore < finalScore)
+        {
+            displayedScore++;
+            resultsScoreText.text = displayedScore.ToString();
+
+            if (countSound != null)
+                countSound.Play();
+
+            yield return new WaitForSeconds(countSpeed);
+        }
+
+        // Reset after showing the total
+        GameData.playerTwoScore = 0;
+    }
+}
