d1df020c2c81dad0d58e7c0cc475613eb36c1820
profoundBoi <connor170103@gmail.com>
2025-12-09 14:58:03 +0200

    TankGame8Started

---
+++ COLOUR_CHASER/Assets/scripts/Game8/TankController.cs
@@ -0,0 +1,115 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
+
+public class TankController : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    private PlayerInput playerInput;
+
+    [Header("Tank Settings")]
+    public float acceleration = 8f;
+    public float reverse = -6f;
+    public float steering = 200f;
+    public float drag = 3f;
+
+
+    private float forwardInput;
+    private float reverseInput;
+    [SerializeField] private float turnInput;
+
+    private SpriteRenderer spriteRnd;
+  
+    [SerializeField]
+    private Sprite Player1, Player2;
+
+    [Header("Spawn Positions (Editable)")]
+    [SerializeField] private GameObject player1Spawn;
+    [SerializeField] private GameObject player2Spawn;
+
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        playerInput = GetComponent<PlayerInput>();
+    }
+
+    private void Start()
+    {
+
+        if (playerInput.playerIndex == 0)
+        {
+            GetComponent<SpriteRenderer>().sprite = Player1;
+            gameObject.tag = "Player1";
+
+            player1Spawn = GameObject.FindGameObjectWithTag("p1");
+            transform.position = player1Spawn.transform.position;
+        }
+        else if (playerInput.playerIndex == 1)
+        {
+            GetComponent<SpriteRenderer>().sprite = Player2;
+            gameObject.tag = "Player2";
+            player2Spawn = GameObject.FindGameObjectWithTag("p2");
+            transform.position = player2Spawn.transform.position;
+        }
+
+        spriteRnd = GetComponent<SpriteRenderer>();
+       
+    }
+
+    public void OnDrive(InputAction.CallbackContext context) => forwardInput = context.ReadValue<float>();
+    public void OnReverse(InputAction.CallbackContext context) => reverseInput = context.ReadValue<float>();
+    public void OnRotate(InputAction.CallbackContext context) => turnInput = context.ReadValue<float>();
+
+
+    void FixedUpdate()
+    {
+        float moveValue = (forwardInput * acceleration) - (reverseInput * reverse);
+        rb.AddForce(transform.up * moveValue);
+
+        float rotationAmount = -turnInput * steering * Time.fixedDeltaTime;
+        rb.MoveRotation(rb.rotation + rotationAmount);
+
+        rb.velocity = rb.velocity * (1 - drag * Time.fixedDeltaTime);
+
+        if (Mathf.Abs(moveValue) > 0.1f)
+        {
+
+        }
+        else
+        {
+
+        }
+    }
+
+    private void Update()
+    {
+  
+    }
+
+    public void OnGameSelection(InputAction.CallbackContext context)
+    {
+        SceneManager.LoadScene("GameSelect");
+    }
+
+    public void ResetToSpawn()
+    {
+        if (playerInput.playerIndex == 0 && player1Spawn != null)
+        {
+            transform.position = player1Spawn.transform.position;
+            rb.velocity = Vector2.zero;
+            rb.angularVelocity = 0f;
+            rb.rotation = 0f;
+        }
+        else if (playerInput.playerIndex == 1 && player2Spawn != null)
+        {
+            transform.position = player2Spawn.transform.position;
+            rb.velocity = Vector2.zero;
+            rb.angularVelocity = 0f;
+            rb.rotation = 0f;
+        }
+    }
+
+}
